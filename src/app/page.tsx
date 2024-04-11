2df9f406cd87f28716aebdf7c6a88b1f5d2331c9
y3owk1n <wongyeowkin@gmail.com>
2024-04-11 17:04:43 +0800

    feat(ui): Add dark mode toggle to home page
    Added a dark mode toggle to the Home page in the Seat Selection Demo.
    The toggle allows users to switch between light, dark, and system themes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import { ModeToggle } from "@/components/dark-mode-toggle";
 import { Seats } from "@/components/seats";
 import { initialSeatData } from "@/lib/seat";
 
 export default function Home(): JSX.Element {
 	return (
 		<main className="container my-10">
-			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-				Seat Selection Demo
-			</h1>
+			<div className="flex gap-4">
+				<h1 className="flex-1 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+					Seat Selection Demo
+				</h1>
+				<ModeToggle />
+			</div>
 			<Seats seats={initialSeatData} />
 		</main>
 	);
