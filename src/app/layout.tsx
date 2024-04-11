bcda2b3ff818df75e443c2f06a21ac37b9741934
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-11 10:43:51 +0800

    chore: satisfy vercel style guide (#10)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
-}>) {
+}>): JSX.Element {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
