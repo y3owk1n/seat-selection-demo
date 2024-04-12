d356500878fe96971d74e56b73af9ae503e7c9ae
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-12 11:11:09 +0800

    refactor(seats): Update seat selection state management (#29)
    Refactor seat selection state management in the Seats component. Remove
    local state for selection error and success ids and utilize the state
    management provided by the useSeatSelection hook. Update references to
    these states accordingly in the Seats component to maintain consistency
    and improve code organization.

--- src/hooks/use-seat-selection.ts
+++ src/hooks/use-seat-selection.ts
@@ -5,15 +5,32 @@ import {
 	pickSeats,
 	toggleSeat,
 } from "@/lib/seat";
-import { useCallback, useState } from "react";
+import {
+	type Dispatch,
+	type SetStateAction,
+	useCallback,
+	useEffect,
+	useState,
+} from "react";
+import { toast } from "sonner";
 
 export function useSeatSelection(seats: Seat[]): {
 	seatsByRow: Record<number, Seat[]>;
 	seatsByColumn: Record<number, Record<number, Seat[]>>;
 	selectedSeat: Seat[];
 	onSelectSeat: (seatId: string) => void;
+	selectionErrorIds: string[];
+	setSelectionErrorIds: Dispatch<SetStateAction<string[]>>;
+	selectionSuccessIds: string[];
+	setSelectionSuccessIds: Dispatch<SetStateAction<string[]>>;
 } {
 	const [selectedSeat, setSelectedSeat] = useState<Seat[]>([]);
+
+	const [selectionErrorIds, setSelectionErrorIds] = useState<string[]>([]);
+
+	const [selectionSuccessIds, setSelectionSuccessIds] = useState<string[]>(
+		[],
+	);
 
 	const seatsByRow = getSeatsByRow(seats);
 
@@ -21,17 +38,47 @@ export function useSeatSelection(seats: Seat[]): {
 
 	const onSelectSeat = useCallback(
 		(seatId: string) => {
+			toast.dismiss();
 			const seat = seats.find((_seat) => _seat.id === seatId);
 			if (!seat) return;
 
 			setSelectedSeat((prevSelectedSeat) =>
 				toggleSeat(prevSelectedSeat, seat),
 			);
-
-			// const res = pickSeats(seats, body.selectedSeatsIds);
 		},
 		[seats],
 	);
 
-	return { seatsByRow, seatsByColumn, selectedSeat, onSelectSeat };
+	useEffect(() => {
+		if (selectedSeat.length > 0) {
+			const res = pickSeats(
+				seats,
+				selectedSeat.map((seat) => seat.id),
+			);
+
+			const filteredErrors = res.filter((r) => !r.success);
+
+			const filteredSuccess = res.filter((r) => r.success);
+
+			if (filteredErrors.length) {
+				setSelectionErrorIds(filteredErrors.map((r) => r.seatId));
+
+				filteredErrors
+					.map((r) => r.message as string)
+					.forEach((error) => toast.error(error));
+			}
+			setSelectionSuccessIds(filteredSuccess.map((r) => r.seatId));
+		}
+	}, [seats, selectedSeat]);
+
+	return {
+		seatsByRow,
+		seatsByColumn,
+		selectedSeat,
+		onSelectSeat,
+		selectionErrorIds,
+		setSelectionErrorIds,
+		selectionSuccessIds,
+		setSelectionSuccessIds,
+	};
 }
