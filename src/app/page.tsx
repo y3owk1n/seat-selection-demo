e8089eec323f2804869f399d85b6c785a748c25d
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-13 16:12:22 +0800

    feat: add section component (#53)
    * feat: add section component
    
    * Apply formatting changes
    
    ---------
    
    Co-authored-by: y3owk1n <y3owk1n@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@
 import { ModeToggle } from "@/components/dark-mode-toggle";
 import { Seats } from "@/components/seats";
 import TicketCard from "@/components/ticket-card";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { useSeatSelection } from "@/hooks/use-seat-selection";
 import { initialSeatData } from "@/lib/seat-data";
