b771c8006a5aa0de53c1f89cb5e148b994f4a86a
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-05-22 11:23:52 +0800

    feat: add loading screen (#75)

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,12 @@
+import { Loader } from "lucide-react";
+
 export default function Loading() {
-	// Or a custom loading skeleton component
-	return <p>Loading...</p>;
+	return (
+		<div className="w-svw h-svh grid place-items-center">
+			<div>
+				<Loader className="size-10 mx-auto mb-4 animate-spin" />
+				<p className="text-center">Loading contents</p>
+			</div>
+		</div>
+	);
 }
