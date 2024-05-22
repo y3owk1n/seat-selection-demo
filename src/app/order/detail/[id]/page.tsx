0a66b130fc9190756b8359dd09f72df01a79170e
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-05-22 10:36:05 +0800

    feat: add back button to pages (#73)

--- src/app/order/detail/[id]/page.tsx
+++ src/app/order/detail/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { ModeToggle } from "@/components/dark-mode-toggle";
 import OrderDetailCard from "@/components/order/order-detail-card";
 import UserInfoBar from "@/components/shared/user-info-bar";
+import { Button } from "@/components/ui/button";
 import { getSessionAndCheckRedirect } from "@/lib/auth";
 import { api } from "@/trpc/server";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface OrderDetailProps {
@@ -29,6 +32,13 @@ export default async function OrderDetail(
 	return (
 		<main className="container max-w-4xl flex flex-col gap-8 py-10 mb-24">
 			<UserInfoBar session={session} />
+			<div>
+				<Button variant="link" size="link" asChild>
+					<Link href="/order">
+						<ArrowLeft className="size-4 mr-2" /> Back to orders
+					</Link>
+				</Button>
+			</div>
 			<div className="flex items-center gap-4">
 				<h1 className="flex-1 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
 					Order Detail
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,8 +1,11 @@
 import { ModeToggle } from "@/components/dark-mode-toggle";
 import OrderList from "@/components/order/order-list";
 import UserInfoBar from "@/components/shared/user-info-bar";
+import { Button } from "@/components/ui/button";
 import { getSessionAndCheckRedirect } from "@/lib/auth";
 import { api } from "@/trpc/server";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function Orders(): Promise<JSX.Element> {
@@ -17,6 +20,13 @@ export default async function Orders(): Promise<JSX.Element> {
 	return (
 		<main className="container max-w-4xl flex flex-col gap-8 py-10 mb-24">
 			<UserInfoBar session={session} />
+			<div>
+				<Button variant="link" size="link" asChild>
+					<Link href="/">
+						<ArrowLeft className="size-4 mr-2" /> Back to home
+					</Link>
+				</Button>
+			</div>
 			<div className="flex items-center gap-4">
 				<h1 className="flex-1 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
 					My Orders
